cde41c8ae1f2dc534c1501ae411d235de31f3032
butlerem <butlerem01@gmail.com>
2024-09-17 19:23:18 -0400

    Wrap feedback widget React component in custom Web Component

--- src/components/Widget.jsx
+++ src/components/Widget.jsx
@@ -35,7 +35,8 @@ export const Widget = () => {
       <Popover>
         <PopoverTrigger asChild>
           <Button className="rounded-full shadow-lg
-          hover:scale-105">Feedback</Button>
+          hover:scale-105">
+            <MessageIcon className="mr-2 h-4.5 w-4.5"/>Feedback</Button>
         </PopoverTrigger>
         <PopoverContent>
           { submitted ? (<div>
@@ -116,4 +117,27 @@ return (
             9.27 8.91 8.26 12 2"/>
         </svg>
     );
+}
+
+function MessageIcon(props) {
+  return (
+    <svg 
+    {...props}
+    xmlns="http://www.w3.org/2000/svg" 
+    width="24" 
+    height="24" 
+    viewBox="0 0 24 24" 
+    fill="none" 
+    stroke="currentColor"
+    stroke-width="2" 
+    stroke-linecap="round" 
+    stroke-linejoin="round" 
+    class="lucide lucide-message-square-more"
+    >
+    <path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"/>
+    <path d="M8 10h.01"/>
+    <path d="M12 10h.01"/>
+    <path d="M16 10h.01"/>
+    </svg>
+  )
 }
